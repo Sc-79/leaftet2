a1965e70d5f138a6de62d4c0979bc2aab3dcc740
sourav <souravchongrey@gmail.com>
2024-09-06 10:04:00 +0530

    find loction, and shorest path, using gleaflet2'

---
+++ mappoint.js
@@ -0,0 +1,62 @@
+const mappoint =[
+    {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+              "coordinates": [
+                73.40275353224473,
+                32.72762828563751
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+              "coordinates": [
+                76.0833846041906,
+                16.214076174020647
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+              "coordinates": [
+                87.87230879228616,
+                22.435633111376077
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+              "coordinates": [
+                80.68686239444924,
+                23.84890891938558
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+              "coordinates": [
+                73.38563163488661,
+                26.096281773688432
+              ],
+              "type": "Point"
+            }
+          }
+        ]
+      }
+]
